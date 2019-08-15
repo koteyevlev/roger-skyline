61cf26a59819389dc2b7d8f4ff11691588b7623d
Sturgis Krystina <skrystin@mi-q2.21-school.ru>
2019-08-15 20:05:29 +0300

    first_try

---
+++ roger-deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	 echo
+	  echo "Shell script roger-deploy : Deploy to remote host."
+	   echo
+	    echo "NAME"
+		 echo " ./roger-deploy -- Automatically deploy the current directory to a remote host with a commit message."
+		  echo
+		   echo "SYNOPSIS"
+		    echo " ./roger-deploy \"Commit message"\"
+			 echo
+			  echo "DESCRIPTION"
+			   echo " This scripts permit auto deployment of the current git repository on a remote server. It commits changes with its first argument then push it. The script then connect to remote server and run a script to retrieve updated files."
+			    echo
+				 echo
+				  echo "EXIT STATUS"
+				   echo " The roger-deploy utility exits 0 on success, and 1 if an error occurs."
+				    echo
+					 echo "EXAMPLES"
+					  echo " The command:"
+					   echo
+					    echo "  ./roger-deploy \"Update my views\""
+						 echo
+						  echo " will commit all changes with this text then push to Github."
+						   exit 1
+					   fi
+
+					   if [ $# -eq 1 ]; then
+						    git add .
+							 git commit -m "$1"
+							  git push
+							   echo "Git repository updated. Now running update on server."
+							    ssh skrystin@192.168.56.3 -p 2222 'bash -s' < update_server.sh
+								 exit 0
+							 fi
+
+							 exit 1
