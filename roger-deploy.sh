812580950d671cd6aa6bc94bd7f3df4add1f9b49
Sturgis Krystina <skrystin@mi-q2.21-school.ru>
2019-08-15 20:33:29 +0300

    first_try

--- roger-deploy.sh
+++ roger-deploy.sh
@@ -31,7 +31,7 @@ if [ $# -eq 0 ]; then
 							 git commit -m "$1"
 							  git push
 							   echo "Git repository updated. Now running update on server."
-							    ssh skrystin@192.168.56.3 -p 2222 "cd /var/www/html/roger-skyline && git pull && exit"
+							    ssh skrystin@192.168.56.3 -p 2222 "cd /var/www/html/roger-skyline && la -la && exit"
 								 exit 0
 							 fi
 
